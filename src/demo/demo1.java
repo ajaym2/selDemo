ef8e6f0e52e8fccbed5deea87abd281ed9f51696
DELL <DELL@DESKTOP-6DDK8C4>
2021-12-25 14:27:31 +0530

    done

--- src/demo/demo1.java
+++ src/demo/demo1.java
@@ -4,26 +4,21 @@ public class demo1 {
 	
 	public static void main(String[] args) {
 		
-		try{
+		try {
 			System.out.println(10/0);
 		}
 		catch(ArithmeticException e)
 		{
-			e.printStackTrace();
-			System.out.println("-------------------------");
-			
-			System.out.println(e.toString());
-			
-			System.out.println("---------------------");
+			//e.printStackTrace();
+			//System.out.println(e);
+			//System.out.println(e.toString());
 			System.out.println(e.getMessage());
+		}
+		}
 		
-		}
-		finally{
-			System.out.println("Project completed");
-		}
 		
 	}
 	
 	
 
-}
+
