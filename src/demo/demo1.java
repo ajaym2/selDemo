ee61b3d6caaa064770e0c824c0adb9f768165a50
ajaym2 <96646048+ajaym2@users.noreply.github.com>
2021-12-25 14:11:49 +0530

    Update demo1.java

--- src/demo/demo1.java
+++ src/demo/demo1.java
@@ -4,24 +4,14 @@ public class demo1 {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("hi hello selenium");
-		int i=15;
-		System.out.println(i);
-		int j;
-		
-		for (j = 0; j < 10; j++) {
-			System.out.println(j);
-			
-			
-			
+		try{
+			System.out.println(10/0);
+		}
+		catch(ArithmeticException e)
+		{
+			e.getMessage();
 		}
 		
-		System.out.println("project completed");
-		
-		
-		System.out.println("how are you bro what happened");
-		
-		System.out.println(i+j);
 	}
 	
 	
